08d872bd089749dfcd4013ccee9f6fdfef553209
Rajendra Wagle <raajen73@gmail.com>
2020-04-06 12:24:46 +0545

    Rebuild attemp 3

---
+++ precache-manifest.4335cce455f49dbec6ce55976282c843.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90bd0b01712b39b23d6d033599369e61",
+    "url": "/corona-tracker/build/index.html"
+  },
+  {
+    "revision": "e292a96ef919120a5e2f",
+    "url": "/corona-tracker/build/static/css/main.380f5856.chunk.css"
+  },
+  {
+    "revision": "63795fd9d7cfe21df891",
+    "url": "/corona-tracker/build/static/js/2.19878cb1.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/build/static/js/2.19878cb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e292a96ef919120a5e2f",
+    "url": "/corona-tracker/build/static/js/main.499621fa.chunk.js"
+  },
+  {
+    "revision": "3de8f6fb15e6ff56e6db",
+    "url": "/corona-tracker/build/static/js/runtime-main.546dc82e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/build/static/media/image.d7265326.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/corona-tracker/precache-manifest.cc853d1246cf9fd466ea77e486d13dea.js"
+  "/corona-tracker/build/precache-manifest.4335cce455f49dbec6ce55976282c843.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/corona-tracker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/corona-tracker/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
